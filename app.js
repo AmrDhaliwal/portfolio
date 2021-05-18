8261bd2ac3a8c1e448e4b0375774224442d0f3b7
AmrDhaliwal <amritdhaliwalz@gmail.com>
2021-05-17 21:46:08 -0700

    ok

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+//app.js
+
 const express = require("express");
 const https = require("https");
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
